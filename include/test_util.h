2cee820c54e7b808001939781c0a53a731c6a682
Chenzs108 <chenzs108@outlook.com>
2022-06-09 01:08:19 +0100

    Feat: Add test utilities

---
+++ include/test_util.h
@@ -0,0 +1,44 @@
+/**
+ * @file test_util.h
+ * @brief Commonly used utilities for unit tests.
+ *
+ * @author Chen Zhenshuo (chenzs108@outlook.com)
+ * @par GitHub
+ * https://github.com/Zhuagenborn
+ * @version 1.0
+ * @date 2022-05-05
+ */
+
+#pragma once
+
+#include "util.h"
+
+#include <string>
+#include <utility>
+
+
+namespace ws::test {
+
+/**
+ * @brief Get the current test name.
+ *
+ * @details
+ * The format is @p SuiteName.CaseName.
+ * @code {.cpp}
+ * TEST(SuiteName, CaseName) {
+ *     // ...
+ * }
+ * @endcode
+ */
+std::string TestName() noexcept;
+
+/**
+ * @brief Create an unique temporary file for the current test.
+ *
+ * @return The file descriptor and path.
+ *
+ * @exception std::system_error Failed to create a temporary file.
+ */
+std::pair<FileDescriptor, std::string> CreateTempTestFile();
+
+}  // namespace ws::test
