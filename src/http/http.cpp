568729587c3b4d7f2c3757e8fdbbbbb7968480f8
Chenzs108 <chenzs108@outlook.com>
2022-06-26 21:00:41 +0100

    Feat: Add the HTTP connection
    Co-authored-by: Liugw <liu.guowen@outlook.com>

--- src/http/http.cpp
+++ src/http/http.cpp
@@ -1,11 +1,32 @@
 #include "http.h"
-#include "util.h"
+#include "io.h"
+#include "request.h"
+#include "response.h"
 
+#include <cassert>
 #include <filesystem>
 #include <sstream>
 
 
 namespace ws::http {
+
+namespace {
+
+std::optional<Parameters> ExtractUserMessage(const Request& request) noexcept {
+    static constexpr std::string_view user_tag {"user"};
+    static constexpr std::string_view msg_tag {"msg"};
+
+    const auto user {request.Post(user_tag).value_or("")};
+    const auto msg {request.Post(msg_tag).value_or("")};
+    if (!user.empty() && !user.empty()) {
+        return Parameters {{user_tag.data(), user.data()},
+                           {msg_tag.data(), msg.data()}};
+    } else {
+        return std::nullopt;
+    }
+}
+
+}  // namespace
 
 std::string_view ContentTypeByFileName(const std::string_view name) noexcept {
     static const std::unordered_map<std::string_view, std::string_view> types {
@@ -133,6 +154,130 @@ std::string PutParamIntoHTML(std::string html, const Parameters& params) {
     }
 
     return html;
+}
+
+std::filesystem::path ConnectionImpl::root_dir_;
+
+void ConnectionImpl::SetRootDirectory(std::filesystem::path dir) noexcept {
+    root_dir_ = std::move(dir);
+}
+
+std::filesystem::path ConnectionImpl::RootDirectory() noexcept {
+    return root_dir_;
+}
+
+ConnectionImpl::ConnectionImpl(const FileDescriptor socket) noexcept :
+    socket_ {socket} {
+    assert(IsValidFileDescriptor(socket_));
+}
+
+ConnectionImpl::~ConnectionImpl() noexcept {
+    Close();
+}
+
+void ConnectionImpl::Close() noexcept {
+    if (IsValidFileDescriptor(socket_)) {
+        close(socket_);
+        socket_ = invalid_file_descriptor;
+    }
+}
+
+FileDescriptor ConnectionImpl::Socket() const noexcept {
+    return socket_;
+}
+
+bool ConnectionImpl::KeepAlive() const noexcept {
+    return keep_alive_;
+}
+
+bool ConnectionImpl::Valid() const noexcept {
+    return IsValidFileDescriptor(socket_);
+}
+
+std::size_t ConnectionImpl::Receive() {
+    io::FileDescriptor io {socket_, socket_};
+    std::size_t size {0};
+
+    try {
+        while (true) {
+            size += read_buf_.ReadFrom(io);
+        }
+    } catch (const std::system_error& err) {
+        if (err.code() != std::errc::resource_unavailable_try_again) {
+            throw;
+        }
+    }
+
+    return size;
+}
+
+std::size_t ConnectionImpl::Send() {
+    io::FileDescriptor io {socket_, socket_};
+    std::size_t header_size {0};
+    while (!write_buf_.Empty()) {
+        header_size += write_buf_.WriteTo(io);
+    }
+
+    std::size_t file_size {0};
+    while (file_size < file_.Size()) {
+        if (const auto size {write(socket_, file_.Data() + file_size,
+                                   file_.Size() - file_size)};
+            size >= 0) {
+            file_size += size;
+        } else {
+            ThrowLastSystemError();
+        }
+    }
+
+    return header_size + file_size;
+}
+
+bool ConnectionImpl::Process() noexcept {
+    static constexpr std::string_view index_page {"/index.html"};
+    static constexpr std::string_view hide_msg_tag {"hide-msg"};
+
+    file_.Unmap();
+    if (read_buf_.ReadableSize() == 0) {
+        return false;
+    }
+
+    Request request;
+    Response response {root_dir_};
+    std::optional<std::string> error_msg;
+
+    try {
+        request.Parse(read_buf_);
+    } catch (const std::exception& err) {
+        error_msg = err.what();
+    }
+
+    keep_alive_ = request.KeepAlive();
+    response.SetKeepAlive(keep_alive_);
+    if (!error_msg.has_value()) {
+        std::string path {request.Path()};
+        if (path.empty() || path == "/") {
+            path = index_page;
+        }
+
+        StatusCode status_code {StatusCode::OK};
+        if (path == index_page) {
+            auto params {ExtractUserMessage(request).value_or(Parameters {})};
+            params.insert({hide_msg_tag.data(), params.empty()
+                                                    ? true_tag.data()
+                                                    : false_tag.data()});
+            response.Build(write_buf_, index_page, params, status_code);
+        } else {
+            if (auto file {
+                    response.Build(write_buf_, std::move(path), status_code)};
+                file.has_value()) {
+                file_ = std::move(file.value());
+            }
+        }
+    } else {
+        response.Build(write_buf_, StatusCode::BadRequest, error_msg.value());
+    }
+
+    return true;
 }
 
 }  // namespace ws::http
